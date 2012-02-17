d0f5944c0df4b2b63174c0cb948829b24086a1af
Firstname Lastname <teosanchez@gmail.com>
2012-02-17 12:07:35 +0100

    jueves 16-nuevo archivo

--- constantes.php
+++ constantes.php
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 <?php
     define ('SERVIDOR','localhost');
     define ('USUARIO','root');
     define ('PASSWORD','');
     define ('BD','lephare4');
-=======
-
-<?php
-    define ('SERVIDOR','localhost');
-    define ('USUARIO','lephare');
-    define ('PASSWORD','sevilla');
-    define ('BD','lephare2');
->>>>>>> 826b6387b62216887e86d83cef3884120dcf5068
 ?>
