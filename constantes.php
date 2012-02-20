7b8b8bf515bd556da3ac4a35b23ddde9481388b5
Firstname Lastname <teosanchez@gmail.com>
2012-02-20 09:06:50 +0100

    modif_lunes

--- constantes.php
+++ constantes.php
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
+
 <?php
     define ('SERVIDOR','localhost');
     define ('USUARIO','root');
     define ('PASSWORD','');
     define ('BD','lephare4');
     define ('REGISTROS_PAGINA',10);
-=======
-<?php
-    define ('SERVIDOR','localhost');
-    define ('USUARIO','root');
-    define ('PASSWORD','');
-    define ('BD','lephare4');
->>>>>>> 524a39d1d31180923aeb50f9f6afd7f6451b992b
+
 ?>
